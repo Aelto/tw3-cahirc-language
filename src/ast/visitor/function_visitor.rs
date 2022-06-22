405cd93e3e5db62b8830b44e0b8c62ec7f2c4a83
Aelto <Aelto@users.noreply.github.com>
2022-06-22 17:15:30 +0200

    Update function_visitor.rs

--- src/ast/visitor/function_visitor.rs
+++ src/ast/visitor/function_visitor.rs
@@ -1,14 +1,16 @@
+use crate::ast::ProgramInformation;
 
-pub struct FunctionVisitor {
 
+pub struct FunctionVisitor<'a> {
+  pub program_information: &'a ProgramInformation,
 }
 
-impl super::Visitor for FunctionVisitor {
-    fn visit_function_declaration(&mut self, node: &mut crate::ast::FunctionDeclaration) {
-        println!("visitor: {:?}", node.name);
-    }
+impl super::Visitor for FunctionVisitor<'_> {
+  fn visit_function_declaration(&mut self, node: &mut crate::ast::FunctionDeclaration) {
+    println!("FunctionVisitor: {:?}", node.name);
+  }
 
-    fn visitor_type(&self) -> super::VisitorType {
-        super::VisitorType::FunctionDeclarationVisitor
-    }
+  fn visitor_type(&self) -> super::VisitorType {
+    super::VisitorType::FunctionDeclarationVisitor
+  }
 }
