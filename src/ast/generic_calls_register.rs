97fb0136fb38a97544c2d949b55bae90b1dbad24
Aelto <Aelto@users.noreply.github.com>
2022-06-22 17:03:32 +0200

    add GenericFunctionsRegister

--- src/ast/generic_calls_register.rs
+++ src/ast/generic_calls_register.rs
@@ -1,10 +1,49 @@
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
 
-/// A list of the types used for a generic call.
-type GenericCall = Vec<String>;
+use super::Expression;
+use super::FunctionDeclaration;
+
+type GenericFunctionName = String;
+
+#[derive(Debug)]
+pub struct GenericFunctionsRegister {
+  pub functions: HashMap<GenericFunctionName, Rc<RefCell<FunctionDeclaration>>>,
+  pub calls: HashMap<GenericFunctionName, GenericCallsRegister>
+}
+
+impl GenericFunctionsRegister {
+  pub fn new() -> Self {
+    Self {
+      functions: HashMap::new(),
+      calls: HashMap::new()
+    }
+  }
+
+  pub fn register_new_generic_function(&mut self, function_name: &str, function: Rc<RefCell<FunctionDeclaration>>) {
+    if !self.calls.contains_key(function_name) {
+      self.calls.insert(function_name.to_string(), GenericCallsRegister::new());
+    }
+
+    self.functions.insert(function_name.to_string(), function);
+  }
+
+  pub fn register_call(&mut self, function_name: &str, call: Vec<String>) {
+    if !self.calls.contains_key(function_name) {
+      self.calls.insert(function_name.to_string(), GenericCallsRegister::new());
+    }
+
+    if let Some(mut call_register) = self.calls.get_mut(function_name) {
+      call_register.register_call(call);
+    }
+  }
+}
 
 #[derive(Debug)]
 pub struct GenericCallsRegister {
-  pub calls: Vec<GenericCall>,
+  pub calls: Vec<Vec<String>>,
 }
 
 impl GenericCallsRegister {
@@ -14,7 +53,7 @@ impl GenericCallsRegister {
     }
   }
 
-  pub fn register_call(&mut self,call: GenericCall) {
+  pub fn register_call(&mut self,call: Vec<String>) {
     if self.has_call_already(&call) {
       return;
     }
@@ -22,12 +61,12 @@ impl GenericCallsRegister {
     self.calls.push(call);
   }
 
-  fn has_call_already(&self, call: &GenericCall) -> bool {
+  fn has_call_already(&self, call: &Vec<String>) -> bool {
     self.calls.iter()
       .any(|self_call| Self::are_calls_identical(self_call, call))
   }
 
-  fn are_calls_identical(a: &GenericCall, b: &GenericCall) -> bool {
+  fn are_calls_identical(a: &Vec<String>, b: &Vec<String>) -> bool {
     !a.iter().zip(b).any(|(a, b)| a != b) 
   }
 }
