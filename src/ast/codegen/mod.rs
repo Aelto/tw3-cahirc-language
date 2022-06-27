e57985803c01d5daf6747bbd651430ad5861536d
Aelto <Aelto@users.noreply.github.com>
2022-06-27 23:09:20 +0200

    prepare the codegen trait

--- src/ast/codegen/mod.rs
+++ src/ast/codegen/mod.rs
@@ -3,10 +3,12 @@ use std::ops::Deref;
 pub mod context;
 
 pub trait Codegen {
-  fn emit(&self, output: &mut Vec<u8>) {
+  fn emit(&self, context: &context::Context, output: &mut Vec<u8>) -> Result<(), std::io::Error> {
     use std::io::Write as IoWrite;
 
-    write!(output, "##default Codegen::emit impl##").unwrap();
+    write!(output, "##default Codegen::emit impl##")?;
+
+    Ok(())
   }
 }
 
@@ -14,10 +16,12 @@ impl<A> Codegen for Vec<A>
 where
   A: Codegen,
 {
-  fn emit(&self, output: &mut Vec<u8>) {
+  fn emit(&self, context: &context::Context, output: &mut Vec<u8>) -> Result<(), std::io::Error> {
     for child in self {
-      child.emit(output);
+      child.emit(context, output)?;
     }
+
+    Ok(())
   }
 }
 
@@ -25,8 +29,8 @@ impl<A> Codegen for std::boxed::Box<A>
 where
   A: Codegen,
 {
-  fn emit(&self, output: &mut Vec<u8>) {
-    self.deref().emit(output);
+  fn emit(&self, context: &context::Context, output: &mut Vec<u8>) -> Result<(), std::io::Error> {
+    self.deref().emit(context, output)
   }
 }
 
@@ -34,8 +38,8 @@ impl<A> Codegen for std::rc::Rc<A>
 where
   A: Codegen,
 {
-  fn emit(&self, output: &mut Vec<u8>) {
-    self.deref().emit(output);
+  fn emit(&self, context: &context::Context, output: &mut Vec<u8>) -> Result<(), std::io::Error> {
+    self.deref().emit(context, output)
   }
 }
 
@@ -43,9 +47,11 @@ impl<A> Codegen for Option<A>
 where
   A: Codegen,
 {
-  fn emit(&self, output: &mut Vec<u8>) {
+  fn emit(&self, context: &context::Context, output: &mut Vec<u8>) -> Result<(), std::io::Error> {
     if let Some(inner) = self {
-      inner.emit(output);
+      inner.emit(context, output)?;
     }
+
+    Ok(())
   }
 }
