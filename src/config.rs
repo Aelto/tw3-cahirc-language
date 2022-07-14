7a6085ced3f360748ddca1eef35823da91ab074c
Aelto <Aelto@users.noreply.github.com>
2022-07-14 11:47:19 +0200

    add support for a path to config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::path::Path;
 
 use serde::Deserialize;
 
@@ -16,8 +17,25 @@ pub struct ConfigPackage {
 }
 
 pub fn read_config() -> std::io::Result<Config> {
-  let config_path = std::path::Path::new("cahirc.toml");
+  let default_path = ".".to_string();
+  let args: Vec<String> = std::env::args().collect();
+  let cwd = Path::new(args.get(2).unwrap_or(&default_path));
+  let config_path = cwd.join("cahirc.toml");
   let content = std::fs::read_to_string(config_path)?;
+
+  let mut config: Config = toml::from_str(&content)?;
+
+  config.package.src = cwd.join(config.package.src).to_str().unwrap().to_string();
+
+  let keys: Vec<String> = config.dependencies.keys().map(String::to_string).collect();
+  for dep_name in keys {
+    let dep_path = config.dependencies.get(&dep_name).unwrap();
+
+    config.dependencies.insert(
+      dep_name.to_string(),
+      cwd.join(dep_path).to_str().unwrap().to_string(),
+    );
+  }
 
   Ok(toml::from_str(&content)?)
 }
